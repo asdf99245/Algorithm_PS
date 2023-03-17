ed2c285af9bffb050c24b4cc638aa731e27ed803
Yunkyu Jung <39851220+asdf99245@users.noreply.github.com>
2023-03-17 15:48:45 +0900

    [unrated] Title: 택배 배달과 수거하기, Time: 0.95 ms, Memory: 9.72 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+long long solution(int cap, int n, vector<int> deliveries, vector<int> pickups) {
+    long long answer = 0;
+    
+    int d = 0;
+    int p = 0;
+    for(int i = n - 1; i >= 0; i--) {
+        d += deliveries[i];
+        p += pickups[i];
+        
+        int cnt = 0;
+        while(d > 0 || p > 0) {
+            d -= cap;
+            p -= cap;
+            cnt++;
+        }
+        answer += (i + 1) * cnt * 2;
+    }
+    
+    return answer;
+}
